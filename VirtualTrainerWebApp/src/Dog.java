d234220b8961c16d802fd13517a03c6b867b4be4
mausam.kumravat <mausam.kumravat@YI1005260DT.Yash.com>
2016-09-14 18:59:31 +0530

    Commited Dog Class Successful

---
+++ VirtualTrainerWebApp/src/Dog.java
@@ -0,0 +1,21 @@
+
+public class Dog {
+private String name;
+private String colour;
+	public Dog() {
+
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+}
